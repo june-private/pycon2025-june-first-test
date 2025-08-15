8330597026d86348ddeaa17f6f615fcbefbaf8ea
june-private <98457880+june-private@users.noreply.github.com>
2025-08-15 06:05:19 +0000

    시작_edit1

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,5 +3,3 @@ import streamlit as st
 st.title("June's Playground")
 st.info("앱을 만들어보겠습니다.")
 st.subheader("어떤 걸 만들지는 모르겠지만")
-
-st.image("https://images.app.goo.gl/impRRaVRvseoqyr58", caption="LFG~")
